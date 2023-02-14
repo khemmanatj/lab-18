5d55e0b85f7c16c9d098d8013ee8db944f14a666
khemmanatj <120445107+khemmanatj@users.noreply.github.com>
2023-02-14 17:01:09 +0700

    Update lab18_2.cpp

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,19 +1,4 @@
-#include<iostream>
-using namespace std;
-
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
-}
-
-int main(){
-	char *p;
-	int N;	
-	cout << "N = ";
-	cin >> N;		
-	myString(p,N);
-	cout << p;	
-	delete [] p;
-	return 0;
+void myString(char *&A,int N){
+	A = new char[N];    
+	for(int i = 0; i < N;i++) A[i] = 'A'+i;
 }
